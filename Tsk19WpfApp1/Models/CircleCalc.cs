f79fd8fbdb50733a608d7623171f298251b9b682
Вадим <Vadimk89@mail.ru>
2022-01-24 23:58:42 +0500

    Добавьте файлы проекта.

---
+++ Tsk19WpfApp1/Models/CircleCalc.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tsk19WpfApp1.Models
+{
+    static class CircleCalc
+    {
+        public static double DiaCalc(double a) => 2 * a;
+        public static double AreCalc(double a) => Math.PI * Math.Pow(a, 2);
+        public static double LenCalc(double a) => 2 * a * Math.PI;
+    }
+}
---
+++ Tsk19WpfApp1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Tsk19WpfApp1.Models;
+
+namespace Tsk19WpfApp1.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+        private double rad;
+        public double Rad
+        {
+            get => rad;
+            set
+            {
+                rad = value;
+                OnPropertyChanged();
+            }
+        }
+        private double diam;
+        public double Diam
+        {
+            get => diam;
+            set
+            {
+                diam = value;
+                OnPropertyChanged();
+            }
+        }
+        private double length;
+        public double Length
+        {
+            get => length;
+            set
+            {
+                length = value;
+                OnPropertyChanged();
+            }
+        }
+        private double area;
+        public double Area
+        {
+            get => area;
+            set
+            {
+                area = value;
+                OnPropertyChanged();
+            }
+        }
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecute(object p)
+        {
+            //Number3 = Number1 + Number2;
+            Diam = CircleCalc.DiaCalc(Rad);
+            Length = CircleCalc.LenCalc(Rad);
+            Area = CircleCalc.AreCalc(Rad);
+        }
+        private bool CanAddCommandExecuted(object p)
+        {
+            //return true;
+            if (Rad != 0)
+                return true;
+            else
+                return
+                    false;
+        }
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecuted);
+        }
+    }
+}
